5e3970f495ea26e2e1573b96e497dc9b2f33b744
Eduardo Hernandez <eduardo.hley@gmail.com>
2020-09-13 18:54:10 -0700

    real logic

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 module.exports.shorten = (name) => {
-  console.log('shortening:', name);
+  const splitted = name.match(/[A-Z]+[^A-Z]*|[^A-Z]+/g).map(s => s.trim());
+
+  if(splitted.length == 1) return splitted[0] + '.';
+  if (splitted.length == 2) return splitted.join(' ') + '.';
+  if (splitted.length > 2) {
+    return splitted.slice(0, 2).concat(...[splitted[2].charAt(0)]).join(' ') + '.';
+  }
 }
